1e4c59f0074028cba483754e39ba26e10e711866
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2022-03-06 00:01:06 +0900

    Controller, lombok, testcode

---
+++ src/test/java/com/surge/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package com.surge.springboot.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}

053584ae4260a1cf37dde9a66301af2725f83087
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2022-03-07 21:13:37 +0900

    머스테치, CRUD

--- src/main/java/com/surge/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/surge/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,10 @@
 package com.surge.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p from Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/surge/springboot/service/posts/PostsService.java
+++ src/main/java/com/surge/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.surge.springboot.service.posts;
 
 import com.surge.springboot.domain.posts.Posts;
 import com.surge.springboot.domain.posts.PostsRepository;
+import com.surge.springboot.web.dto.PostsListResponseDto;
 import com.surge.springboot.web.dto.PostsResponseDto;
 import com.surge.springboot.web.dto.PostsSaveRequestDto;
 import com.surge.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        postsRepository.delete(posts);
     }
 }

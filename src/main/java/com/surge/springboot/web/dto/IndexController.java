ee42ddf6eea5ff1c93d65dcd8c94a61b8c3c0534
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2022-03-08 01:00:55 +0900

    OAuth2.0, 구글로그인

--- src/main/java/com/surge/springboot/web/dto/IndexController.java
+++ src/main/java/com/surge/springboot/web/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.surge.springboot.web.dto;
 
+import com.surge.springboot.config.auth.dto.SessionUser;
 import com.surge.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user !=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 

4a31d238577925ccd8165292a55d5513de80173b
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2022-03-09 00:20:00 +0900

    OAuth2.0, 네이버로그인

--- src/main/java/com/surge/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/surge/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.surge.springboot.config.auth.dto;
 import com.surge.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
